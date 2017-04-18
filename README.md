4b81d4d130a675cf3ff78db03c083859d468d092
timrobertson100 <timrobertson100@gmail.com>
2017-04-18 22:03:54 +0200

    Adds Kafka example

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This code demonstrates streaming bulk load into HBase `1.2.x` using Spark `1.6.x
 A Netcat process is used to simulate a stream of data.
 
 To run this example:
-   1. Start an HBase server in standalone mode by  [downloading version 1.2.5](http://mirrors.dotsrc.org/apache/hbase/1.2.5/hbase-1.2.5-bin.tar.gz) and then issuing `./bin/start-hbase.sh`.  Verify it works by checking the HBase Master UI console is visible on [http://localhost:16010/master-status](http://localhost:16010/master-status).
+   1. Start an HBase server in standalone mode by [downloading version 1.2.5](http://mirrors.dotsrc.org/apache/hbase/1.2.5/hbase-1.2.5-bin.tar.gz) and then issuing `./bin/start-hbase.sh`.  Verify it works by checking the HBase Master UI console is visible on [http://localhost:16010/master-status](http://localhost:16010/master-status).
    
    2. Create the table in HBase:
 ```
@@ -16,8 +16,18 @@ hbase(main):034:0> create 'streamingtest', {NAME => 'c', VERSIONS => 1}
   
    4. The project `pom` contains a profile named `ide` which allows you to run the example in Intellij by simply enabling the `ide` profile and then right-click `Bulkload` and `Run Bulkload` (this assumes an HBase cluster is running locally).
 
-       
 Each entry writen in the Netcat console should result in a new row in HBase.  The rows are written in bulk every 10 seconds. 
+
+To run the example using Kafka, follow the above and then
+
+   5. Start a Kafka server locally by [downloading version 2.10-0.10.2.0](http://mirrors.rackhosting.com/apache/kafka/0.10.2.0/kafka_2.10-0.10.2.0.tgz) and then issuing `./bin/kafka-server-start.sh config/server.properties`
+    
+   6. Create a topic by issuing `./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test`
+   
+   7. Open a producer by issuing `./bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test`
+   
+   8. Right-click `Bulkload4` and `Run Bulkload4`   
+       
 
 
   
---
+++ src/main/scala/com/opencore/sample/BulkLoad4.scala
@@ -0,0 +1,81 @@
+package com.opencore.sample
+
+import kafka.serializer.StringDecoder
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.spark.HBaseContext
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable.{Map => MMap}
+
+/**
+  * A version of the Bulkload demonstrating that the DStream can use the HBaseRow container Java object reading from
+  * Kafka.
+  * Here the first word of the kafka message becomes the HBase rowKey, while the remainder become columns c1,c2,c3 etc.
+  */
+object BulkLoad4 {
+
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Bulkload example")
+    conf.setIfMissing("spark.master", "local[2]")
+    val sc = new SparkContext(conf)
+    try {
+
+      // For the simplest example we connect to a socket on port 7001 and then micro batch content at 10 second intervals
+      val ssc = new StreamingContext(sc, Seconds(10))
+
+      val kafkaParams = Map[String, String](
+        "metadata.broker.list" -> "localhost:9092",
+        "group.id" -> "test"
+      )
+
+      val topicSet = Set("test")
+
+      val input = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicSet)
+
+      // Convert the stream into a stream simulating HBaseRow objects
+      // This will split the line text into words, and the first word becomes the key
+      val indexRows : DStream[HBaseRow] = input.map(message => {
+        var row = new HBaseRow
+        row.setColumnFamily("c")
+
+        // first column becomes the key, the rest just cells
+        message._2.split(" ").zipWithIndex.foreach{
+          case(word,i) => {
+            if (i==0) row.setRowKey(word.getBytes())
+            else row.getColumns.put("c"+i, word.getBytes())
+          }
+        }
+        row
+      })
+
+
+      // There will be a streaming bulk put issued every 10 seconds
+      val hbaseContext = new HBaseContext(sc, HBaseConfiguration.create())
+      hbaseContext.streamBulkPut[HBaseRow](
+        indexRows,
+        TableName.valueOf("streamingtest"),
+        (indexRow: HBaseRow) => {
+          val put: Put = new Put(indexRow.getRowKey)
+
+          indexRow.getColumns.asScala.map{ case(columnName, value) => {
+            put.addColumn(indexRow.getColumnFamily.getBytes(), columnName.getBytes, value)
+          }}
+
+          put
+        }
+      )
+
+      ssc.start()
+      ssc.awaitTerminationOrTimeout(600000) // run the demo for 10 minute
+
+    } finally {
+      sc.stop()
+    }
+  }
+}
